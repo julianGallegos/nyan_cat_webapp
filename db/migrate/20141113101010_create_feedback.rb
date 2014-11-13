2bdab6ee6daad0433cc02f302207dbd2a2903810
julian <julian.gallegos@gmail.com>
2014-11-13 15:28:21 -0800

    fixed ajax bug for feedback page

--- db/migrate/20141113101010_create_feedback.rb
+++ db/migrate/20141113101010_create_feedback.rb
@@ -3,6 +3,7 @@ class CreateFeedback < ActiveRecord::Migration
     create_table :feedbacks do |t|
       t.string :email
       t.text :content
+      t.integer :nyan, default: 0
     end
   end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -2,8 +2,15 @@ $(document).ready(function() {
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
+  bindEvents()
+});
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+function bindEvents(){
+  $("#feedback_form").on("click", addNewEntry);
+}
+
 
 var addNewEntry = function(event){
   event.preventDefault();
@@ -13,15 +20,12 @@ var addNewEntry = function(event){
     data: {email: this.email.value, content: this.content.value}
   });
   ajaxRequest.done(function(server_data){
-
-    $("#new_entry").append(server_data.content + '<br><br>')
-
+    console.log("yo")
+    $("#new_entry").append(server_data.content + '<br>')
   }
 )};
 
-  $("#feedback_form").on("click", addNewEntry);
 
 
 
-});
 
